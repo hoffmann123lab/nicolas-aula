51766efc6f5a0eff6df801ca85349e8998438198
Vitor Ferreira <vitorlubeck@gmail.com>
2026-06-02 09:12:32 -0300

    aula_67

---
+++ aula_6/jv.js
@@ -0,0 +1,11 @@
+const form - document.querySelector('#cadastro').value
+
+function AoClicar("submit" async (event)){
+
+    event.preventDefault();
+
+    const dados = {
+        nome
+    }
+
+}
---
+++ aula_7/jv.js
@@ -0,0 +1,38 @@
+// let frutas = ["Maçã", "Uva", "Pêra"]
+
+// console.log(frutas, frutas.length)
+
+// let alunos = [
+//     {
+//         id: 1,
+//         nome: "murilo",
+//         sobrenome: "chaves"
+//     },
+//     {
+//         id: 2,
+//         nome:"vinicius",
+//         sobrenome: "lima"
+//     },
+//     {
+//         id: 3,
+//         nome: "Davi",
+//         sobrenome: "Ferrari"
+//     }   
+// ]
+
+// const inf = alunos.map(alunos => `${alunos.id}"-"${alunos.nome} ${alunos.sobrenome}`)
+
+
+// console.log(inf, alunos.length)
+
+
+// function soma(n1, n2){
+//     return n1 + n2
+// }
+
+// console.log(soma(10,20))
+// console.log(soma(50,30))
+function exibirNome(){
+    let nome = document.querySelector('#nome').value
+    alert(nome)
+}
